a6c32fea28a6521319e1ef85338bca523afa62d2
Ben Jacquez <ben@Bens-MBP.attlocal.net>
2023-02-19 11:37:43 -0800

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      { text: "Hyper Text Machine Language", correct: false },
+      { text: "Hyper Text Markup Language", correct: true },
+      { text: "Hard Technical Markup Launcher", correct: false },
+      { text: "Hyper Technical Machine Lancher", correct: false }
+    ]
+  },
+  {
+    question: "What symbols are used to comment on javascript?",
+    answers: [
+      { text: "//", correct: true },
+      { text: "/* */", correct: false },
+      { text: "# #", correct: false },
+      { text: "<!-- -->", correct: false }
+    ]
+  },
+  {
+    question: "How would you link a .js file to a .html?",
+    answers: [
+      { text: "<script href=", correct: false },
+      { text: "<link href=", correct: false },
+      { text: "<script src=", correct: true },
+      { text: "<link href=", correct: false }
+    ]
+  },
+  {
+    question: "?",
+    answers: [
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: false },
+      { text: "", correct: true }
+    ]
+  }
+];
